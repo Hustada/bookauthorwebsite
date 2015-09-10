1cfc15ac8b73556cc5e18ce6b46516b44aa27212
Mark Hustad <mark_hustad@Marks-MacBook.local>
2015-09-09 22:40:09 -0500

    added recent posts to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
     def show
       @user = User.find(params[:id])
-      @posts = @user.posts
+      @posts = @user.posts.paginate(:page => params[:page], :per_page => 5)
+
     end
 
     def create
