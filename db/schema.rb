e6559343d5271569261a7491d2757fca7730c304
Mark Hustad <mark_hustad@Marks-MacBook.local>
2015-09-22 00:27:36 -0500

    almost done with admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904191819) do
+ActiveRecord::Schema.define(version: 20150922044431) do
 
   create_table "comments", force: true do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150904191819) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean  "admin",             default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  "Mark Hustad",
+             email: "hustada80@gmail.com",
+             password:              "frackers",
+             password_confirmation: "frackers",
+						 admin: true,
+						 activated: true)
 
 99.times do |n|
   name  = Faker::Name.name
