4d4e7ccb56fc85e72b2afdbbb65016029c35e93f
Mark Hustad <mark_hustad@Marks-MacBook.local>
2015-10-22 20:58:38 -0500

    need to commit something here, destroyed email lists for now

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ActiveRecord::Base
 	validates :body,  presence: true
 	has_many :comments, dependent: :destroy
 	belongs_to :user
+
+	auto_html_for :body do
+    html_escape
+    image
+    youtube(:width => 400, :height => 250, :autoplay => true)
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
 
+  get '/home' =>  'static_pages#home'
   get '/about' => 'static_pages#about'
   get '/author' => 'static_pages#author'
   get '/contact' => 'static_pages#contact'
