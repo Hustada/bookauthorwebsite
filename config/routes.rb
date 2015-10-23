a69ce692efc8049dc51d3acfe02cc87f3f89171f
Mark Hustad <mark_hustad@Marks-MacBook.local>
2015-10-23 01:44:11 -0500

    almost done with home page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   resources :posts
-  root 'posts#index'
+  root 'static_pages#home'
 
   get '/home' =>  'static_pages#home'
   get '/about' => 'static_pages#about'
