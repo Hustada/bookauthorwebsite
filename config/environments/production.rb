ae1912d28ff66f175169af5d6b039c8fc6665f07
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-03-14 00:23:34 -0500

    trying to get emails to send with actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendmail
+  Defaults to:
+  config.action_mailer.sendmail_settings = {
+    location: '/usr/sbin/sendmail',
+    arguments: '-i -t'
+  }
   config.action_mailer.default_url_options = { host: 'https://protected-gorge-9156.herokuapp.com' }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
