e2a80df6542b6441503641d7b15106064b77c8fd
Mark Hustad <mark_hustad@Marks-MacBook.local>
2016-03-14 22:18:36 -0500

    remove unneeded lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,12 +11,11 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
